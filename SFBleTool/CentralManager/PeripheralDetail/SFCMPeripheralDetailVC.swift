fd8c0721fd97a582977ed0d835c3389b280125e7
hsf <hsf89757@gmail.com>
2024-09-06 09:08:51 +0800

    SegmentView

--- SFBleTool/CentralManager/PeripheralDetail/SFCMPeripheralDetailVC.swift
+++ SFBleTool/CentralManager/PeripheralDetail/SFCMPeripheralDetailVC.swift
@@ -59,20 +59,9 @@ class SFCMPeripheralDetailVC: SFViewController {
     // MARK: ui
     private lazy var segmentView: SFCMPeripheralDetailSegmentView = {
         return SFCMPeripheralDetailSegmentView().then { view in
-            view.indicatorView = SFSegmentIndicatorDotView()
+            view.indicatorView = SFSegmentIndicatorSquareView()
             view.didSelectedItemBlock = {
                 [weak self] segmentView, index in
-                
-                if index == 0 {
-                    segmentView.indicatorView = SFSegmentIndicatorLineView()
-                }
-                else if index == 1 {
-                    segmentView.indicatorView = SFSegmentIndicatorDotView()
-                }
-                else {
-                    segmentView.indicatorView = SFSegmentIndicatorArrowView()
-                }
-                
                 self?.advVc.view.isHidden = index != 0
                 self?.serviceVc.view.isHidden = index != 1
                 self?.logVc.view.isHidden = index != 2
