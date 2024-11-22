e5cae88463ea13783e8a873f85ac5978efefe6df
hsf89757 <hsf89757@gmail.com>
2024-11-22 22:42:07 +0800

    调试登录页面

--- SFBleTool/AppDelegate.swift
+++ SFBleTool/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // Resource
+        SFImage.App.icon = UIImage(named: "AppIcon")
+        SFText.App.name = "BleTool"
+        SFText.App.slogen = "蓝牙通信，畅享每一刻。"
         
         // 日志
         SFLogger.config()
