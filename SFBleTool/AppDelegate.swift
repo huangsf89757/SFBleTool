092298199fd22511103fe6bacef375a5d97dbb8f
hsf89757 <hsf89757@gmail.com>
2024-11-23 11:56:25 +0800

    本地化

--- SFBleTool/AppDelegate.swift
+++ SFBleTool/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        
+        
         // Resource
         SFImage.App.icon = UIImage(named: "AppIcon")
         SFText.App.name = "BleTool"
