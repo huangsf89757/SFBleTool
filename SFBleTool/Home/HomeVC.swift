ed1849eff740d70b407785f93310745536007bda
hsf89757 <hsf89757@gmail.com>
2024-12-25 20:29:21 +0800

    svg

--- SFBleTool/Home/HomeVC.swift
+++ SFBleTool/Home/HomeVC.swift
@@ -16,6 +16,7 @@ import SFUI
 import SFLogger
 // Third
 import SideMenu
+import Macaw
 
 // MARK: - HomeVC
 class HomeVC: SFViewController {
@@ -47,7 +48,35 @@ class HomeVC: SFViewController {
     
     // MARK: func
     func settingAction() {
+        let svg = SVGView()
+//        svg.node = 
         
+    
+//        // 获取私有库的 Bundle
+//        let privateLibraryBundle = Bundle.module
+//
+//        // 加载 SVG 文件数据
+//        if let svgURL = privateLibraryBundle.url(forResource: "myIcon", withExtension: "svg"),
+//           let svgData = try? Data(contentsOf: svgURL),
+//           let svgNode = try? SVGParser.parse(data: svgData) {
+//            let macawView = MacawView(node: svgNode, frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//            view.addSubview(macawView)
+//        } else {
+//            print("无法加载 myIcon.svg 文件的数据")
+//        }
+//        
+//        // 获取私有库的 Bundle
+//        let privateLibraryBundle = Bundle.module // Swift Package Manager 提供的默认方法
+//
+//        // 加载 SVG 文件路径
+//        if let svgPath = privateLibraryBundle.path(forResource: "myIcon", ofType: "svg") {
+//            let svgView = try! SVGParser.parse(path: svgPath)
+//            let macawView = MacawView(node: svgView, frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//            view.addSubview(macawView)
+//        } else {
+//            print("无法找到 myIcon.svg 文件")
+//        }
+
     }
 }
 
